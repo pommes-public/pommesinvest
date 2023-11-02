0dcc7516a1e38188a834d24dfa3d47fcf9f96fff
Johannes Kochems <jokochems@web.de>
2023-11-02 16:37:03 +0100

    Update corresponding author

--- pommesinvest/model_funcs/data_input.py
+++ pommesinvest/model_funcs/data_input.py
@@ -5,7 +5,7 @@ General description
 This file contains all function definitions for reading in input data
 used for the investment variant of POMMES.
 
-@author: Johannes Kochems (*), Johannes Giehl (*), Yannick Werner,
+@author: Johannes Kochems (*), Johannes Giehl, Yannick Werner,
 Benjamin Grosse
 
 Contributors:
@@ -13,7 +13,7 @@ Julien Faist, Hannes Kachel, Sophie Westphal, Flora von Mikulicz-Radecki,
 Carla Spiller, Fabian Büllesbach, Timona Ghosh, Paul Verwiebe,
 Leticia Encinas Rosa, Joachim Müller-Kirchenbauer
 
-(*) Corresponding authors
+(*) Corresponding author
 """
 
 import numpy as np
